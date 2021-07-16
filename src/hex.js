57645caaa25fe9740dfc9fa14755b7f0f0e87bd6
Odebisi Solomon idowu <Odebisiidowu09@gmail.com>
2021-07-16 15:36:58 +0100

    adding files

--- src/hex.js
+++ src/hex.js
@@ -1,6 +1,6 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-const button = document.getElementById("btn");
+const button = document.querySelector(".btn");
 const main = document.getElementsByTagName("main")[0];
 const color = document.getElementsByTagName("span")[0];
 
